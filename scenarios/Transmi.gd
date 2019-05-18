2c3b50d685856c959139318536513cf74ae5484b
Mateo Robayo <mapedorr@gmail.com>
2019-05-18 02:15:52 -0500

    Added initial progress bars for Travel and Doors timers

--- scenarios/Transmi.gd
+++ scenarios/Transmi.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
+export(bool) var debugging = false
+
 onready var secs = 0
 onready var elapsed_secs = 0
+onready var doors_open = false
+onready var _ui = $UI
+onready var win = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,20 +28,35 @@ func finish_level(body):
 	if body.name == "Player":
 		$Mx.stop()
 		$Mx_Win.play()
-		print("Level finished!")
+		$Travel.stop()
+
+	# Check if we're debugging
+	if debugging:
+		$Debug.show()
+
 
 func setup_time():
 	secs = $Bus.travel_time + $Bus.doors_time
-	$UI/GameTime.set_text("%02d" % secs)
+	if debugging:
+		$Debug/GameTime.set_text("%02d" % secs)
+	_ui.initialize($Bus.travel_time, $Bus.doors_time)
+	# Listen signals and start the timer
 	$Travel.connect("timeout", self, "travel_timeout")
 	$Travel.start()
 
 func travel_timeout():
+	# Update the UI
+	if not doors_open:
+		_ui.update_travel_progress()
+	else:
+		_ui.update_doors_progress()
+
 	secs -= 1
-	elapsed_secs += 1
-	if elapsed_secs == $Bus.travel_time:
-		$UI/GameTime.add_color_override("font_color", Color(1, 0, 0, 1))
 	if secs == 0:
 		$Travel.stop()
-		print("The game has finished")
-	$UI/GameTime.set_text("%02d" % secs)
+		print("You are a loser")
+	elapsed_secs += 1
+	if elapsed_secs == $Bus.travel_time:
+		doors_open = true
+	if debugging:
+		$Debug/GameTime.set_text("%02d" % secs)
---
+++ scenarios/scripts/UI.gd
@@ -0,0 +1,18 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Doors.hide()
+
+func initialize(travel_time, doors_time):
+	$Travel/Progress.max_value = travel_time
+	$Doors/Progress.max_value = doors_time
+	$Doors/Progress.value = doors_time
+
+func update_travel_progress(magnitude = 1):
+	$Travel/Progress.value += magnitude
+
+func update_doors_progress(magnitude = 1):
+	if not $Doors.visible:
+		$Doors.show()
+	$Doors/Progress.value -= magnitude
