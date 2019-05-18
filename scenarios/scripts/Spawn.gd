65a4c341a3a35fc6aec3a81b9033cbd3812eca21
Mateo Robayo <mapedorr@gmail.com>
2019-05-17 22:08:34 -0500

    Bus scene first steps
    The Spawn puts the player in its position.

---
+++ scenarios/scripts/Spawn.gd
@@ -0,0 +1,7 @@
+extends Sprite
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Set the player position here
+	$"../../Player".position = get_position()
+	self.hide()
