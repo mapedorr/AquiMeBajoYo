faf799f7dda50e9b2448f3d66fef84158e0b13d9
agar3s <ekeisco@gmail.com>
2019-05-17 22:20:52 -0500

    integrate main animal with transmi scene

--- scenarios/scripts/Spawn.gd
+++ scenarios/scripts/Spawn.gd
@@ -3,5 +3,5 @@ extends Sprite
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Set the player position here
-	$"../../Player".position = get_position()
+	#$"../../Player".position = get_position()
 	self.hide()
