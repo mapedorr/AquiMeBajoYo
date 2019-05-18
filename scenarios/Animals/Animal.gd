5c15f01c9c7a2e6477cc0107374d7aaca05e6e89
agar3s <ekeisco@gmail.com>
2019-05-18 04:05:05 -0500

    update borders

--- scenarios/Animals/Animal.gd
+++ scenarios/Animals/Animal.gd
@@ -6,7 +6,7 @@ func add_force(force):
 	velocity = force
 
 func _physics_process(delta):
-	velocity = velocity*1
+	velocity = velocity*0.9
 	var collision_info = move_and_collide(velocity*delta)
 	if collision_info and collision_info.collider.is_in_group('Animals'):
 		var collision_point = collision_info.position
