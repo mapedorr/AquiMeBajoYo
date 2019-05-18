c524373d1dc13ed4a7cac2ec4638acfc4d6f16dd
Mateo Robayo <mapedorr@gmail.com>
2019-05-18 00:45:48 -0500

    Travel and door timers working

--- scenarios/Trasnsmi.gd
+++ scenarios/Trasnsmi.gd
@@ -1,14 +1,31 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var secs = 0
+onready var elapsed_secs = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Animal.position = $Bus/Spawn.get_position()
 	$Bus.connect("exit_entered", self, "finish_level")
+	# Setup the timers
+	self.setup_time()
 
 func finish_level(body):
 	if body.name == "Animal":
 		print("Level finished!")
+
+func setup_time():
+	secs = $Bus.travel_time + $Bus.doors_time
+	$UI/GameTime.set_text("%02d" % secs)
+	$Travel.connect("timeout", self, "travel_timeout")
+	$Travel.start()
+
+func travel_timeout():
+	secs -= 1
+	elapsed_secs += 1
+	if elapsed_secs == $Bus.travel_time:
+		$UI/GameTime.add_color_override("font_color", Color(1, 0, 0, 1))
+	if secs == 0:
+		$Travel.stop()
+		print("The game has finished")
+	$UI/GameTime.set_text("%02d" % secs)
--- scenarios/scripts/Bus.gd
+++ scenarios/scripts/Bus.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 signal exit_entered
 
+# The time it takes the bus to arrive at the station (in segundos)
+export(int) var travel_time = 20
+# The time the bus keeps its doors opened (in segundos)
+export(int) var doors_time = 30
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Spawn.hide()
